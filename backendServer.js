9cc3195604d059e4776cc4eabf24030bf979715c
Martin Smitnya <smitnyamartin@gmail.com>
2021-01-21 15:36:34 +0100

    heroku deploy

--- backendServer.js
+++ backendServer.js
@@ -130,6 +130,6 @@ app.delete('/posts/:id', (req, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log('Server is ONLINE at 3000');
+app.listen(5000, () => {
+  console.log('Server is ONLINE at 5000');
 });
